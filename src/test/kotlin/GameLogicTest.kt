9ce6617e0f47c2a0703fad8f3bc418ac971fd729
spakkrishnakumar <akhil.krishnakumar@springernature.com>
2021-05-02 17:30:40 +0530

    Create failing test - 'should return possible moves for Queen'

--- src/test/kotlin/GameLogicTest.kt
+++ src/test/kotlin/GameLogicTest.kt
@@ -1,5 +1,4 @@
 import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
 
 class GameLogicTest {
@@ -26,5 +25,18 @@ class GameLogicTest {
         assertThat(actual, containsAllOf(expected))
     }
 
+    @Test
+    internal fun `should return possible moves for Queen`() {
+        val expected = listOf(
+            "E5", "E6", "D6", "C6", "C5", "C4", "D4", "E4",
+            "F5", "F7", "D7", "B7", "B5", "B3", "D3", "F3",
+            "G5", "G8", "D8", "A8", "A5", "A2", "D2", "G2",
+            "H5", "D1", "H1"
+        )
+        val input = Queen("D5")
 
+        val actual = gameLogic(input)
+
+        assertThat(actual, containsAllOf(expected))
+    }
 }
