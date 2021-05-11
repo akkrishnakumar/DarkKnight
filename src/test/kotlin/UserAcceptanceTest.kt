c3dac43897740a5db505c897ba28c605af510215
spakkrishnakumar <akhil.krishnakumar@springernature.com>
2021-05-11 21:53:33 +0530

    Create failing acceptance test - 'Queen can move across the board is all 8 directions'

--- src/test/kotlin/UserAcceptanceTest.kt
+++ src/test/kotlin/UserAcceptanceTest.kt
@@ -27,6 +27,21 @@ class UserAcceptanceTest {
     }
 
     @Test
+    fun `Queen can move across the board is all 8 directions`() {
+        val expected = listOf(
+            "E5", "E6", "D6", "C6", "C5", "C4", "D4", "E4",
+            "F5", "F7", "D7", "B7", "B5", "B3", "D3", "F3",
+            "G5", "G8", "D8", "A8", "A5", "A2", "D2", "G2",
+            "H5", "D1", "H1"
+        )
+        val input = "Queen D5"
+
+        val actual = board.showMoves(input).successValue()
+
+        assertThat(actual, containsAllOf(expected))
+    }
+
+    @Test
     fun `Show Error message when wrong input is entered`() {
         val expected = ParsingError("Wrong Input")
         val input = "Wrong Input"
