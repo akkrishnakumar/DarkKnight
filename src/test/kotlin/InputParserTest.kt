63f4269c322b6e2ff2e3f5884b5655bab297b5b0
spakkrishnakumar <akhil.krishnakumar@springernature.com>
2021-05-11 22:21:09 +0530

    Modify to data driven test - 'should return success result with Piece when input is valid'

--- src/test/kotlin/InputParserTest.kt
+++ src/test/kotlin/InputParserTest.kt
@@ -1,16 +1,18 @@
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments.of
+import org.junit.jupiter.params.provider.MethodSource
+
 
 class InputParserTest {
 
     private val parser = DefaultInputParser()
 
-    @Test
-    internal fun `should return success result with Piece when input is valid`() {
-        val expected = King("D5")
-        val input = "King D5"
-
+    @ParameterizedTest
+    @MethodSource("validInputCombinations")
+    internal fun `should return success result with Piece when input is valid`(input: String, expected: Piece) {
         val actual = parser(input).successValue()
 
         assertThat(actual, equalTo(expected))
@@ -24,5 +26,14 @@ class InputParserTest {
         val actual = parser(input).failureValue()
 
         assertThat(actual, equalTo(expected))
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun validInputCombinations() = listOf(
+            of("King D5", King("D5")),
+            of("Queen D5", Queen("D5")),
+        )
     }
 }
